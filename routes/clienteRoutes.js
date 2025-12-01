acf55c4de788896f59062ddd27f4e510e96b2a85
unknown <giovanna.v.nascimento7@aluno.senai.br>
2025-12-01 14:44:19 -0300

    commit geral do projeto

--- routes/clienteRoutes.js
+++ routes/clienteRoutes.js
@@ -4,8 +4,8 @@ const  clienteController  = require('../controllers/clienteController');
 
 clienteRoutes.get('/clientes', clienteController.selecionaTodosClientes);
 clienteRoutes.post('/clientes', clienteController.criarCliente);
-// clienteRoutes.put('/clientes/atualizatelefone', clienteController.atualizaTelefone)
-// clienteRoutes.put('/clientes/atualizacliente/:id', clienteController.atualizaCliente)
+ clienteRoutes.put('/clientes/atualizatelefone', clienteController.atualizaTelefone)
+ clienteRoutes.put('/clientes/atualizacliente/:id', clienteController.atualizaCliente)
 clienteRoutes.delete('/clientes/deletarcliente/:id', clienteController.DeleteCliente)
 
 module.exports = { clienteRoutes };
